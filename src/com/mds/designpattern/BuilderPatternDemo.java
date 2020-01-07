76c77335406e92c194cfe7407e7b4a4d26000d8c
Jérémy <jerem.blcn@gmail.com>
2020-01-07 11:54:08 +0100

    test

---
+++ src/com/mds/designpattern/BuilderPatternDemo.java
@@ -0,0 +1,7 @@
+package com.mds.designpattern;
+
+public class BuilderPatternDemo {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/com/mds/designpattern/Meal.java
@@ -0,0 +1,5 @@
+package com.mds.designpattern;
+
+public class Meal {
+
+}
